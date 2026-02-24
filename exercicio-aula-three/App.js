195fe8ecf6c6529c4ae05923988b97ef4ff04b75
Francisco Alamino Neto <franciscoalaminoneto26@gmail.com>
2026-02-24 10:53:43 -0300

    feat: add exercicio-aula-three

---
+++ exercicio-aula-three/App.js
@@ -0,0 +1,15 @@
+let nome = "Francisco";
+let idade = 20;
+let cidade = "São Paulo";
+
+let numero1 = 20;
+let numero2 = 5;
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos. Moro em " + cidade + ".");
+
+console.log("Soma:", numero1 + numero2);
+console.log("Subtração:", numero1 - numero2);
+console.log("Multiplicação:", numero1 * numero2);
+console.log("Divisão:", numero1 / numero2);
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
